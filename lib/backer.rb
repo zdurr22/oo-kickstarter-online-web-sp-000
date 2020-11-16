975043d84b27fcf130fe7e522480ccff34202c75
Zachary Durr <zdurr22@gmail.com>
2020-11-16 14:59:17 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,16 @@
+class Backer
+  attr_accessor :name
+  attr_reader :backed_projects
+
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+
+
+  def back_project(project)
+    @backed_projects << project
+    project.add_backer(self) unless project.backers.include?(self)
+  end
+
+end
---
+++ lib/project.rb
@@ -0,0 +1,17 @@
+require 'pry'
+class Project
+  attr_accessor :title
+  attr_reader :backers
+
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+
+  def add_backer(backer)
+    @backers << backer
+    backer.back_project(self) unless backer.backed_projects.include?(self)
+  end
+
+
+end
