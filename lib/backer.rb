c4e0f54dfe89dc22edbea6122fec742bf2375349
Zachary Durr <zdurr22@gmail.com>
2020-11-16 14:53:09 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,7 +1,9 @@
 class Backer
+  attr_accessor :name
   attr_reader :backed_projects
 
-  def initialize
+  def initialize(name)
+    @name = name
     @backed_projects = []
   end
 
@@ -9,5 +11,5 @@ class Backer
   def backed_projects(project)
     @backed_projects << project
   end
-  
+
 end
