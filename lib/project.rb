5bf3a4a602f73b0ecf2f841ba864887bc480eb44
Zachary Durr <zdurr22@gmail.com>
2020-11-16 14:53:39 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,10 +1,10 @@
 require 'pry'
 class Project
-  attr_accessor :name
+  attr_accessor :title
   attr_reader :backers
 
-  def initialize(name)
-    @name = name
+  def initialize(title)
+    @title = title
     @backers = []
   end
 
